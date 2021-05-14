7dbad528721da393ab9b586f94a82665670f6be3
MaryamNona <>
2021-05-14 22:15:34 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Maryam Nona
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function addAgeClicked () {
+  // this function does area and perimeter 
+
+  // input
+  const length = parseInt(document.getElementById('length-of-rectangle').value)
+  const width = document.getElementById('width-of-rectangle').value
+
+  // process
+  const area = length * width
+  const perimeter = 2 * (length + width)
+
+  // output
+document.getElementById('area').innerHTML = 'Area is: ' + length + ' x ' + width + ' = ' + '.'
+document.getElementById('perimeter').innerHTML = 'perimeter is: 2 x (' + length + ' + ' + width + ')' + ' = '
+}
